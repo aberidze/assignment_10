226f4e294cb266ca8fce61224fc37d39b0e96252
Anri_Beridze <aberidze02@gmail.com>
2023-10-23 03:44:19 +0400

    Initial Commit

---
+++ Assignment_10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_10
+//
+//  Created by Macbook Air 13 on 23.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
